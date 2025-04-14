1d8fab871fd47e0730f56d7f44dda3ed42e83b97
klayhung <klayhung@gmail.com>
2025-04-14 16:44:20 +0800

    新增 ConsoleApp 專案及非同步執行範例

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+List<Task> tasks =
+[
+    RunAsync(),
+    RunAsync(),
+    RunAsync(),
+    RunAsync(),
+    RunAsync(),
+    RunAsync(),
+];
+
+await Task.WhenAll(tasks);
+
+static async Task RunAsync()
+{
+    Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+    await Task.Delay(2000);
+}
