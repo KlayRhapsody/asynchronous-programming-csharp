97268164c6bb9a1632a919c62e8c5dc32b2d850d
klayhung <klayhung@gmail.com>
2025-04-14 17:19:16 +0800

    加入 Task.Run 來執行非同步方法 (用到多個 Thread)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,12 +7,20 @@ List<Task> tasks =
     RunAsync(),
     RunAsync(),
     RunAsync(),
+    RunAsync(),
+    RunAsync(),
+    RunAsync(),
+    RunAsync(),
+    RunAsync(),
 ];
 
 await Task.WhenAll(tasks);
 
-static async Task RunAsync()
+static Task RunAsync()
 {
-    Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
-    Thread.Sleep(2000);
+    return Task.Run(() =>
+    {
+        Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(2000);
+    });
 }
